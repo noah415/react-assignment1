530a9aff8aa5d0d7789747ef8e49f7c4f8c4354f
noah415 <noah.otsuka@gmail.com>
2021-09-27 13:36:20 -0700

    added backend directory

---
+++ backend/sample_backend.py
@@ -0,0 +1,52 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+users = { 
+   'users_list' :
+   [
+      { 
+         'id' : 'xyz789',
+         'name' : 'Charlie',
+         'job': 'Janitor',
+      },
+      {
+         'id' : 'abc123', 
+         'name': 'Mac',
+         'job': 'Bouncer',
+      },
+      {
+         'id' : 'ppp222', 
+         'name': 'Mac',
+         'job': 'Professor',
+      }, 
+      {
+         'id' : 'yat999', 
+         'name': 'Dee',
+         'job': 'Aspring actress',
+      },
+      {
+         'id' : 'zap555', 
+         'name': 'Dennis',
+         'job': 'Bartender',
+      }
+   ]
+}
+
+@app.route('/users')
+def get_users():
+  search_username = request.args.get('name')
+
+  if search_username :
+    subdict = {'users_list' : []}
+    for user in users['users_list']:
+      subdict['users_list'].append(user)
+    return subdict
+  return users
+
+  @app.route('/users/<id>')
+  def get_user(id):
+    if id:
+      for user in users['users_list']:
+        
