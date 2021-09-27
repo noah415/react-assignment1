f585cb6a7505c233c57fe742d1ee4b90a848325e
noah415 <noah.otsuka@gmail.com>
2021-09-27 15:16:23 -0700

    added the backend api code for GET, POST, and DELETE requests

--- backend/sample_backend.py
+++ backend/sample_backend.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+from flask import jsonify
 
 app = Flask(__name__)
 
@@ -34,19 +35,43 @@ users = {
    ]
 }
 
-@app.route('/users')
+@app.route('/users', methods=['GET', 'POST', 'DELETE'])
 def get_users():
-  search_username = request.args.get('name')
-
-  if search_username :
-    subdict = {'users_list' : []}
-    for user in users['users_list']:
-      subdict['users_list'].append(user)
-    return subdict
-  return users
-
-  @app.route('/users/<id>')
-  def get_user(id):
-    if id:
+   if request.method == 'GET':
+      search_username = request.args.get('name')
+      search_job = request.args.get('job')
+      if search_username :
+         subdict = {'users_list' : []}
+         for user in users['users_list']:
+            if search_username == user['name']:
+               if search_job:
+                  if search_job == user['job']:
+                     subdict['users_list'].append(user)
+               else:
+                  subdict['users_list'].append(user)
+         return subdict
+      return users
+   elif request.method == 'POST':
+      userToAdd = request.get_json()
+      users['users_list'].append(userToAdd)
+      resp = jsonify(success=True)
+      #resp.status_code = 200 #optionally, you can always set a response code. 
+      # 200 is the default code for a normal response
+      return resp
+   elif request.method == 'DELETE':
+      delete_username = request.args.get('name')
       for user in users['users_list']:
-        
+         if user['name'] == delete_username:
+            del users['users_list'][user]
+      resp = jsonify(success=True)
+      return users
+
+@app.route('/users/<id>')
+def get_user(id):
+   if id:
+      for user in users['users_list']:
+         if user['id'] == id:
+           return user
+      return ({})
+   return users
+
